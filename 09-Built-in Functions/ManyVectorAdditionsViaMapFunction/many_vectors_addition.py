86b1d0a9992a5b93553252c24fae110d10848273
HC <hakan.cuzdan@yandex.com>
2019-10-18 13:25:43 +0300

    vector name changed to vector_key

--- 09-Built-in Functions/ManyVectorAdditionsViaMapFunction/many_vectors_addition.py
+++ 09-Built-in Functions/ManyVectorAdditionsViaMapFunction/many_vectors_addition.py
@@ -17,18 +17,18 @@ def add_many_vectors(**kwargs):
     # make sure that all the vectors have the same length, otherwise we cannot add them
     # if not, throw an error
     vector_length = None
-    for vector in kwargs:  # for each key (i.e. named 'vector') in the dictionary kwargs
+    for vector_key in kwargs:  # for each key (i.e. named 'vector') in the dictionary kwargs
         if vector_length is None:  # if it is the first vector
-            vector_length = len(kwargs[vector])
-        elif vector_length != len(kwargs[vector]):
+            vector_length = len(kwargs[vector_key])
+        elif vector_length != len(kwargs[vector_key]):
             raise AssertionError  # vectors passed cannot have different lengths for addition purposes
 
     # if we get to here, that means we have vectors having equal length
     # we can add them via map function
     if vector_length is not None:
         total = [0]*vector_length  # i.e. [0, 0, 0] --> itself a vector
-        for vector in kwargs:  # for each key (i.e. named as vector) in kwargs
-            total = list(map(lambda x,y: x+y, total, kwargs[vector]))
+        for vector_key in kwargs:  # for each key (i.e. named as vector) in kwargs
+            total = list(map(lambda x,y: x+y, total, kwargs[vector_key]))
     else:  # kwargs did not contain any vectors
         total = []
     return total
