021ea5c75f7af0d0d3f3d92375509d533e06b235
HC <hakan.cuzdan@yandex.com>
2019-10-26 12:44:03 +0300

    Problem1-gensquares : my_range was a container, now it is an iterator

--- 11-Python Generators and Iterators/Problem1-gensquares/test_my_range.py
+++ 11-Python Generators and Iterators/Problem1-gensquares/test_my_range.py
@@ -4,39 +4,59 @@ import gensquares
 
 class TestMyRange(unittest.TestCase):
     def test_N_is_minus_two(self):
-        test_iterator = gensquares.my_range(N=-2)  # iterator is my_range(N)
-        print(test_iterator)
-        self.assertEqual(next(test_iterator), [])
+        """
+        >>> iterable = range(-2)
+        >>> iterator = iter(iterable)
+        >>> next(iterator)
+        Traceback (most recent call last):
+          File "<stdin>", line 1, in <module>
+        StopIteration
+        """
+        test_iterator = gensquares.my_range(-2)  # iterator is my_range(N)
         with self.assertRaises(StopIteration):
             next(test_iterator)
 
     def test_N_is_minus_one(self):
-        test_iterator = gensquares.my_range(N=-1)  # iterator is my_range(N)
-        self.assertEqual(next(test_iterator), [])
+        """
+        >>> iterable = range(-1)
+        >>> iterator = iter(iterable)
+        >>> next(iterator)
+        Traceback (most recent call last):
+          File "<stdin>", line 1, in <module>
+        StopIteration
+        """
+        test_iterator = gensquares.my_range(-1)  # iterator is my_range(N)
         with self.assertRaises(StopIteration):
             next(test_iterator)
 
     def test_N_is_zero(self):
-        test_iterator = gensquares.my_range(N=0)  # iterator is my_range(N)
-        self.assertEqual(next(test_iterator), [])
+        """
+        >>> iterable = range(0)
+        >>> iterator = iter(iterable)
+        >>> next(iterator)
+        Traceback (most recent call last):
+          File "<stdin>", line 1, in <module>
+        StopIteration
+        """
+        test_iterator = gensquares.my_range(0)  # iterator is my_range(N)
         with self.assertRaises(StopIteration):
             next(test_iterator)
 
     def test_N_is_one(self):
-        test_iterator = gensquares.my_range(N=1)  # iterator is my_range(N)
+        test_iterator = gensquares.my_range(1)  # iterator is my_range(N)
         self.assertEqual(next(test_iterator), 0)
         with self.assertRaises(StopIteration):
             next(test_iterator)
 
     def test_N_is_two(self):
-        test_iterator = gensquares.my_range(N=2)  # iterator is my_range(N)
+        test_iterator = gensquares.my_range(2)  # iterator is my_range(N)
         self.assertEqual(next(test_iterator), 0)
         self.assertEqual(next(test_iterator), 1)
         with self.assertRaises(StopIteration):
             next(test_iterator)
 
     def test_N_is_three(self):
-        test_iterator = gensquares.my_range(N=3)  # iterator is my_range(N)
+        test_iterator = gensquares.my_range(3)  # iterator is my_range(N)
         self.assertEqual(next(test_iterator), 0)
         self.assertEqual(next(test_iterator), 1)
         self.assertEqual(next(test_iterator), 2)
