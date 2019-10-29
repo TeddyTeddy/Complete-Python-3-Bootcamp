6e9bd0ac806948417fcd15f219fb2b9d2b7ca9c3
HC <hakan.cuzdan@yandex.com>
2019-10-29 11:03:50 +0200

    my_map can also use lambda expressions

--- 09-Built-in Functions/my_map/my_map.py
+++ 09-Built-in Functions/my_map/my_map.py
@@ -62,6 +62,8 @@ if __name__ == '__main__':
     list_1 = [1, 2, 3]  # an iterable
     list_2 = [5, 2, 4]  # an iterable
     list_3 = [7, 7, 7]  # an iterable
+
+    # USING A FUNCTION (i.e. get_total)
     builtin_map_instance = map(get_total, list_1, list_2, list_3)     # built-in map class usage, returns an iterator
     print('__next__' in dir(builtin_map_instance))  # True, built-in map returns a generator, which is an iterator
     print('__iter__' in dir(builtin_map_instance))  # True, built-in map iterator is an iterable
@@ -74,3 +76,15 @@ if __name__ == '__main__':
     print(my_map_generator == iter(my_map_generator))  # True, proof that my_map returns itself as its own iterator
     print(list(my_map_generator))  # Yes! list class excepts our own "my_map" iterator
 
+    # USING LAMBDA EXPRESSION instead of get_total
+    builtin_map_instance = map(lambda x, y, z: x+y+z, list_1, list_2, list_3)  # built-in map class usage, returns an iterator
+    print('__next__' in dir(builtin_map_instance))  # True, built-in map returns a generator, which is an iterator
+    print('__iter__' in dir(builtin_map_instance))  # True, built-in map iterator is an iterable
+    print(builtin_map_instance == iter(builtin_map_instance)) # True, map instance returns itself as iterator
+    print(list(builtin_map_instance))
+
+    my_map_generator = my_map(lambda x, y, z: x+y+z, list_1, list_2, list_3)  # own my_map usage
+    print('__next__' in dir(my_map_generator))  # True, proof that my_map_generator is an iterator
+    print('__iter__' in dir(my_map_generator))  # True, proof that my_map_generator is an iterable
+    print(my_map_generator == iter(my_map_generator))  # True, proof that my_map returns itself as its own iterator
+    print(list(my_map_generator))  # Yes! list class excepts our own "my_map" iterator
