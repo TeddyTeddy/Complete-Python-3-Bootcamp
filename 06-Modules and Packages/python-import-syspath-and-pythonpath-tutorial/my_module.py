221db79f69f12e36acdd79638511f2e3e3ca7862
TeddyTeddy <hakan.cuzdan@yandex.com>
2020-03-02 17:35:00 +0200

    Tutorial added : Python import, sys.path, and PYTHONPATH

---
+++ 06-Modules and Packages/python-import-syspath-and-pythonpath-tutorial/my_module.py
@@ -0,0 +1,14 @@
+# my_module.py
+print('This will run when the file is imported or invoked.')
+print(f'__name__ is {__name__}')
+
+
+def my_function():
+    print('Executing function. This will only run when the function is called.')
+
+
+if __name__ == '__main__':
+    print('This will get executed only if')
+    print('the module is invoked directly.')
+    print('It will not run when this module is imported')
+    my_function()
